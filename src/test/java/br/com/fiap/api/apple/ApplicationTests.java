23c44639c8f97545c9979e5e0b93414d261ef7eb
Felipe Torlai <felipe.c.torlai@gmail.com>
2024-08-13 19:53:32 -0300

    first commit

---
+++ src/test/java/br/com/fiap/api/apple/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
